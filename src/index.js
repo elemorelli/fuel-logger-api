851ffca5013a7be5cf3e90fb048c6b5116ae25e1
Eleazar Morelli Sangil <gemorelli@gmail.com>
2019-11-06 13:38:45 -0300

    Warning for missing Env Variables

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 const app = require("./app");
+const requiredEnvVariables = ["PORT", "MONGODB_URL", "JWT_SECRET"];
+
+requiredEnvVariables.forEach((envVariable) => {
+    if (!process.env[envVariable]) {
+        console.error(`WARNING: Environment variable '${envVariable}' is required`);
+    }
+});
+
 const port = process.env.PORT;
 
 app.listen(port, () => {
